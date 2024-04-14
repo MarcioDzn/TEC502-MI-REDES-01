59c51b9f092d4eaa68397b11805cff85dc194789
MarcioDzn <marcio.roberto0739@gmail.com>
2024-04-14 14:05:47 -0300

    :recycle: refactor: change time style

--- Server/utils.py
+++ Server/utils.py
@@ -4,4 +4,17 @@ from datetime import datetime
 def get_current_time():
     curr_time = datetime.now()
 
-    return f"{curr_time.hour}:{curr_time.minute}:{curr_time.second}"
+    hour = curr_time.hour
+    if (hour < 10):
+        hour = f"0{hour}"
+
+    minute = curr_time.minute
+    if (minute < 10):
+        minute = f"0{minute}"
+        
+    second = curr_time.second
+    if (second < 10):
+        second = f"0{second}"
+
+
+    return f"{hour}:{minute}:{second}"
