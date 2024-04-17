bfccc1d7f10b34f3928fcd58ce61b995dff970b6
MarcioDzn <marcio.roberto0739@gmail.com>
2024-04-17 20:46:11 -0300

    :sparkles: feat: add "i'm connected" message
    device sends a message via udp to the broker saying that it is connected

--- Server/Client.py
+++ Server/Client.py
@@ -18,19 +18,21 @@ class Client:
                 while not server_on:
                     try:
                         client_sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
                         client_sock_tcp.connect(self.address)
 
                         server_on = True
+
                     except Exception as e:
                         pass
 
                     sleep(3) # 3 segundos 
 
 
-                request = client_sock_tcp.recv(1024).decode().strip()
+                request = client_sock_tcp.recv(1024).decode().strip() 
 
                 if not request:
-                    break
+                    server_on = False
                 
                 self.device.handle_requests(request)
 
@@ -43,11 +45,22 @@ class Client:
 
         client_sock_tcp.close()    
 
+    def send_alive_check(self, sock):
+        while True:
+            try:
+                response = "alive_check online"
+                sock.sendall(response.encode())
+            except:
+                pass
+            finally:
+                sleep(10)
+
 
     def send_response(self):
         client_sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client_sock_udp.connect((self.address[0], self.address[1] + 2000)) # porta 5000
 
+        threading.Thread(target=self.send_alive_check, args=(client_sock_udp,), name="alive_checker").start()
         while True:
             try:
                 data = str(self.device.get_data())
@@ -61,18 +74,18 @@ class Client:
                     
                     if self.device.status == "unpaused":
                         sent_pause_message = False
-                        response = f"{response} online"
+                        response = f"data {response} online"
                         client_sock_udp.sendall(response.encode())
 
                     elif self.device.status == "paused":
                         if not sent_pause_message:
-                            response = f"{response} paused"
+                            response = f"data {response} paused"
                             client_sock_udp.sendall(response.encode())
                         sent_pause_message = True
 
 
                 elif not self.device.online:
-                    response = f"{self.device.name} {time} offline offline"
+                    response = f"data {self.device.name} {time} offline offline"
 
                     if not sent_off_message:
                         client_sock_udp.sendall(response.encode())
@@ -80,8 +93,7 @@ class Client:
 
 
             except Exception as e:
-                print("Erro durante o envio de dados:", e)
-                break
+                pass
 
 
 # device = AirConditioner("Temperatura")
