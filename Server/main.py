0a8f51daaeeca406d01e6032eccbe49e19238314
MarcioDzn <marcio.roberto0739@gmail.com>
2024-04-20 14:29:49 -0300

    :wastebasket: remove: "pause"/"unpause" options

--- Server/main.py
+++ Server/main.py
@@ -7,29 +7,19 @@ client = Client(("localhost", 3000), device)
 
 def questions():
     while True:
-        print("\n[1] Conectar (Não implementado)")
-        print("[2] Desconectar (Não implementado)")
-        print("[3] Ligar dispositivo")
-        print("[4] Desligar dispositivo")
-        print("[5] Pausar dispositivo")
-        print("[6] Despausar dispositivo")
-        print("[7] Alterar valor padrão\n")
+        print("\n[1] Ligar dispositivo")
+        print("[2] Desligar dispositivo")
+        print("[3] Alterar valor padrão\n")
 
         resp = int(input("Escolha uma opcao: "))
 
-        if (resp == 3):
+        if (resp == 1):
             client.device.handle_requests("turn_on")
 
-        elif (resp == 4):
+        elif (resp == 2):
             client.device.handle_requests("turn_off")
-            
-        elif (resp == 5):
-            client.device.handle_requests("pause")
 
-        elif (resp == 6):
-            client.device.handle_requests("unpause")
-
-        elif (resp == 7):
+        elif (resp == 3):
             new_value = input("Informe o novo valor: ")
             client.device.set_default_value(new_value)
 
